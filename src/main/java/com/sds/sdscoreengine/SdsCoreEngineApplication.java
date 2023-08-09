a8b5e36eaf1d5b312e05208001d7c858619ceac4
Sahoo, Pradeep Kumar <pradsahoo@deloitte.com>
2023-08-09 13:53:17 +0530

    initial commit

---
+++ src/main/java/com/sds/sdscoreengine/SdsCoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sds.sdscoreengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsCoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsCoreEngineApplication.class, args);
+	}
+
+}
