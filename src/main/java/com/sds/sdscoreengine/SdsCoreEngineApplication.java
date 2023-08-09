84a17197b9425a04763b041b69a07d7185dbb3d8
Sahoo, Pradeep Kumar <pradsahoo@deloitte.com>
2023-08-09 22:05:16 +0530

    Fake commit

--- src/main/java/com/sds/sdscoreengine/SdsCoreEngineApplication.java
+++ src/main/java/com/sds/sdscoreengine/SdsCoreEngineApplication.java
@@ -1,9 +1,11 @@
 package com.sds.sdscoreengine;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SdsCoreEngineApplication {
 
 	public static void main(String[] args) {
